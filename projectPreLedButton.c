0b2bbe6af965fbe2b85c65f3ff364499b12ff70e
ctowne20 <30992628+ctowne20@users.noreply.github.com>
2017-09-27 22:44:13 +0000

    Update projectPreLedButton.c

--- projectPreLedButton.c
+++ projectPreLedButton.c
@@ -4,26 +4,26 @@
 #define ButtonPin 1
 int main(void)
 {
-	if(wiringPiSetup() == -1)
-	{ //when initialize wiring failed,print messageto screen
-		printf("setup wiringPi failed !");
+	if(wiringPiSetup() < 0)
+	{ 
+		printf("wiringPi setup failed");
 		return 1; 
 	}
 
 	pinMode(LedPin, OUTPUT); 
 	pinMode(ButtonPin, INPUT);
 	pullUpDnControl(ButtonPin, PUD_UP); 
-	//pull up to 3.3V,make GPIO1 a stable level
+
 	while(1)
 	{
 		digitalWrite(LedPin, HIGH);
 		if(digitalRead(ButtonPin) == 0)
 		{
-			//indicate: that button has pressed down
 			digitalWrite(LedPin, LOW); //led on
+			printf("Button is pressed);
 		}
 	
 	}
 	return 0;
 }
-//code from SunFounder
+// pieces of code from SunFounder
